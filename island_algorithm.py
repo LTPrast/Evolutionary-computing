9f74eff6408fe62e668df5866d6c615d45fe2641
agolombek <91390904+agolombek@users.noreply.github.com>
2022-09-22 19:04:11 +0200

    island_model_plus_first_test

---
+++ island_algorithm.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 21 13:05:26 2022
+
+@author: arong
+"""
+
+# imports framework
+import sys
+sys.path.insert(0, 'evoman')
+from environment import Environment
+from demo_controller import player_controller
+
+# imports other libs
+import time
+import numpy as np
+from math import fabs,sqrt
+import glob, os
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# import evolutionary algorithm functions
+from parent_selection_methods import *
+from mutation_recombination_methods import *
+from survival_methods import *
+from island_functions import *
+
+# choose this for not using visuals and thus making experiments faster
+headless = True
+if headless:
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+experiment_name = 'first_real_run'
+if not os.path.exists(experiment_name):
+    os.makedirs(experiment_name)
+
+# runs simulation
+def simulation(env,x):
+    f,p,e,t = env.play(pcont=x)
+    return f
+
+# evaluates fitness of every individual in population
+def evaluate(x):
+    return np.array(list(map(lambda y: simulation(env,y), x)))
+
+
+####################### SET EXPERIMENT PARAMETERS ###########################
+
+np.random.seed(99)
+
+n_hidden_neurons = 10
+
+opponents = [4]
+difficulty = 2
+
+# initializes simulation in individual evolution mode, for single static enemy.
+env = Environment(experiment_name=experiment_name,
+                  enemies=opponents,
+                  playermode="ai",
+                  player_controller=player_controller(n_hidden_neurons),
+                  enemymode="static",
+                  level=difficulty,
+                  speed="fastest")
+
+# default environment fitness is assumed for experiment
+env.state_to_log() # checks environment state
+
+
+# genetic algorithm params
+run_mode = 'train' # train or test
+
+# number of weights for multilayer with 10 hidden neurons
+n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
+
+
+dom_u = 1                               # Max weight for neural network
+dom_l = -1                              # Min weight for neural network
+dist_std = 0.1                          # mean of distribution to draw sizes for gene mutation
+npop = 200                              # Population size
+num_sub_pop = 5                         # number of subpopulations
+gens = 65                               # number of generations
+individuals_deleted = 40                # number of individuals killed every generation
+num_offspring = individuals_deleted     # equal number of offspring to keep constant population size
+tournament_size = 4                     # Number of individuals taking part in tournamnet selection 
+sigma = 0.1                             # gene mutation probability 
+migration_rate = 10                     # every how many generations communication occurs between subpopulation
+migration_magnitude = 2                 # how many members cross over to a new island
+
+#################### PREFORM EXPERIMENT #####################################
+
+start_time = time.time()
+
+# number of times for this experiment to be repeated
+experiment_iterations = 10
+
+average_fitness_data = np.empty((0, gens+1), float)
+max_fitness_data = np.empty((0,gens+1), float)
+best_solution_data = np.empty((0,n_vars), float)
+
+for iteration in range(experiment_iterations):
+
+    # Create Populations, one individual is one row in a matrix
+    population = np.random.uniform(dom_l, dom_u, (npop, n_vars))
+      
+    # find fitness of each member of the initial population
+    fit_pop = evaluate(population)
+    subpop_plot_data = np.array([fit_pop])
+    
+    best_solution_index = np.argmax(fit_pop)
+    # find the fitness of the best solution
+    fitness_of_best_solution = [fit_pop[best_solution_index]]
+    # find the mean fitness of the population
+    mean = [np.mean(fit_pop)]
+    
+    for generation in range(gens):
+        
+        population, fit_pop = island_mutations(population, fit_pop, num_sub_pop, num_offspring, tournament_size, dist_std, sigma, evaluate)
+        
+        if generation != 0 and generation % migration_rate == 0:
+           population, fit_pop = island_migration(population, fit_pop, num_sub_pop, tournament_size, migration_magnitude)
+    
+        subpop_plot_data = np.vstack((subpop_plot_data, fit_pop))
+            
+        best_solution_index = np.argmax(fit_pop)
+        fitness_of_best_solution.append(fit_pop[best_solution_index])
+        mean.append(np.mean(fit_pop))
+
+    
+    plot_sub_populations(subpop_plot_data, num_sub_pop, gens, iteration, max_fit=True)
+    plot_sub_populations(subpop_plot_data, num_sub_pop, gens, iteration, max_fit=False)
+    
+    average_fitness_data = np.append(average_fitness_data, [np.array(mean)], axis=0)
+    max_fitness_data = np.append(max_fitness_data,  [np.array(fitness_of_best_solution)], axis=0)
+    best_solution_data = np.append(best_solution_data, [np.array(population[best_solution_index][:])], axis=0)
+
+end_time = time.time()
+
+execution_time = ((end_time-start_time)/60)/60
+print("Runtime was ", execution_time, " hours")
+
+##################### Save Data in Files ####################################
+
+def save_file(data, file_name, experiment_name, cols=True, rows=True):
+    # Save the data into a dataframe and save in csv file
+    df = pd.DataFrame(data)
+
+    if cols == True:
+        columns = ['Generation_'+str(i+1) for i in range(gens+1)]
+        df.columns = columns
+    if rows == True:
+        rows = ['Trial_'+str(i+1) for i in range(experiment_iterations)]
+        df.index = rows
+    df.to_csv(experiment_name+'/'+experiment_name+file_name)
+    return
+
+# One file to store set-up
+
+set_up_dict = {
+  "Population size": npop,
+  "Generations": gens,
+  "Killed per generation": individuals_deleted,
+  "Offspring number": num_offspring,
+  "Tournament size": tournament_size,
+  "Gene mutation probability": sigma,
+  "Enemies": opponents,
+  "Level": difficulty,
+  "Iterations:":experiment_iterations,
+  "Experiment Name": experiment_name,
+  "Execution Time": execution_time,
+  "Migration Rate": migration_rate,
+  "Migration Magnitude": migration_magnitude,
+  "Number of subpopulations": num_sub_pop
+}
+
+save_file(set_up_dict, '_set_up.csv', experiment_name, cols=False, rows=False)
+
+# One file for mean fitness, max fitness, standard deviation and best solution
+save_file(average_fitness_data, '_mean_fitness.csv', experiment_name)
+save_file(max_fitness_data, '_max_fitness.csv', experiment_name)
+save_file(best_solution_data, '_best_solution.csv', experiment_name, cols=False)
---
+++ island_functions.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 21 12:14:25 2022
+
+@author: arong
+"""
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from parent_selection_methods import *
+from mutation_recombination_methods import *
+from survival_methods import *
+
+
+def island_mutations(population, fit_pop, num_sub_pop, num_offspring, tournament_size, dist_std, sigma, evaluate):
+    """
+    population = population of all islands
+    fit_pop = fitness of all individulas of all islands
+    num_sub_pop = number of sub population i.e. islands 
+    num_offspring = total number of offspring across all islands
+    tournament_size = tournament size on each island
+    dist_std = standard deviation of chosen distribution
+    sigma = mutation probability 
+    evaluate = evaluation function
+    """
+    
+    offspring_per_island = int(num_offspring/num_sub_pop)
+    index_increment = int(len(fit_pop)/num_sub_pop)
+    start = 0
+    stop = index_increment
+    
+    # iterate through all subpopulations
+    for x in range(num_sub_pop):
+        
+        sub_pop = population[start:stop][:]
+        fit_sub_pop = fit_pop[start:stop]
+        
+        # create offspring for sub-population
+        for y in range(int(offspring_per_island/2)):
+            
+            # select parents of subpopulation
+            parent_1 = tournament_selection(sub_pop, fit_sub_pop, tournament_size)
+            parent_2 = tournament_selection(sub_pop, fit_sub_pop, tournament_size)
+            
+            if y % 2 == 0:
+                
+                child_1, child_2 = simple_arithmetic_recombination(parent_1, parent_2)
+                child_1 = gaussian_mutation(child_1, sigma, dist_std)
+                child_2 = gaussian_mutation(child_2, sigma, dist_std)
+            
+            else:
+                child_1 = gaussian_mutation(parent_1, sigma, dist_std)
+                child_2 = gaussian_mutation(parent_2, sigma, dist_std)
+            
+            # evaluate each child
+            child_1_fitness = evaluate([child_1])[0]
+            child_2_fitness = evaluate([child_2])[0]
+            
+            # find worst indidivudals of subpopulation
+            index_sorted = np.argsort(fit_sub_pop)
+            # find local (subpopulation level) and global (population level) index of worst member of subpopulation
+            worst_idx_local = index_sorted[0]
+            worst_idx_global = worst_idx_local + start
+            # find local (subpopulation level) and global (population level) index of 2nd worst member of subpopulation
+            worst2_idx_local = index_sorted[-1]
+            worst2_idx_global = worst2_idx_local + start
+            
+            # replace worst member of subpopulation if child has higher fitness
+            if child_1_fitness > fit_pop[worst_idx_global]:
+                population[worst_idx_global][:] = child_1
+                fit_pop[worst_idx_global] = child_1_fitness
+                
+                if child_2_fitness > fit_pop[worst2_idx_global]:
+                    population[worst2_idx_global][:] = child_2
+                    fit_pop[worst2_idx_global] = child_2_fitness
+            
+            elif child_2_fitness > fit_pop[worst_idx_global]:
+                population[worst_idx_global][:] = child_2
+                fit_pop[worst_idx_global] = child_2_fitness
+    
+        start += index_increment
+        stop += index_increment
+    
+    return population, fit_pop
+
+
+def island_migration(population, fit_pop, num_sub_pop, tournament_size, migration_magnitude):
+    
+    index_increment = int(len(fit_pop)/num_sub_pop)
+    stop = index_increment
+    
+    starting_islands = np.arange(0, num_sub_pop, 1)
+    destination_islands = np.arange(0, num_sub_pop, 1)
+    np.random.shuffle(destination_islands)
+
+    for x in range(num_sub_pop):
+        
+        # check that starting point and destination are not the same
+        if starting_islands[x] != destination_islands[x]:
+            
+            start_island_start = starting_islands[x]*index_increment
+            start_island_stop = start_island_start + index_increment
+            
+            destination_island_start = destination_islands[x]*index_increment
+            destination_island_stop = destination_island_start + index_increment
+            
+            start_island_pop = population[start_island_start:start_island_stop][:]
+            start_island_fit = fit_pop[start_island_start:start_island_stop]
+            
+            destination_island_pop = population[destination_island_start:destination_island_stop][:]
+            destination_island_fit = fit_pop[destination_island_start:destination_island_stop]
+
+            
+            for y in range(migration_magnitude):
+                
+                # find migrant using tournament selection
+                migrant, migrant_idx = tournament_selection_migration(start_island_pop, start_island_fit, tournament_size)
+                migrant_fitness = start_island_fit[migrant_idx]
+                
+                # find worst individual in destination island
+                index_sorted = np.argsort(destination_island_fit)
+                worst_individual_local_index = index_sorted[0]
+
+                worst_individual_global_index = destination_island_start + worst_individual_local_index
+                
+                
+                population[worst_individual_global_index][:] = migrant
+                fit_pop[worst_individual_global_index] = migrant_fitness
+    
+    return population, fit_pop
+            
+
+def tournament_selection_migration(population, fit_pop, k):
+    """
+    This function preforms a tournament selection of the population, the inputs
+    are the population, the fitness of the population and the tournamnet size.
+    
+    The function return the winner of the tournamanet.
+    """
+    # pick random index of population and set current winner to this indes
+    max_idx = len(fit_pop)
+    parent_idx = np.random.randint(0, max_idx)
+
+    # preform k direct comparissons to random members of population, if one has
+    # a higher fitness than the current member, update the current winner index.
+    for i in range(k):
+        rnd_idx = np.random.randint(0, max_idx)
+
+        if fit_pop[rnd_idx] > fit_pop[parent_idx]:
+            parent_idx = rnd_idx
+            
+    # return the parent according to the winning index
+    parent = population[parent_idx][:]
+
+    return parent, parent_idx
+                
+
+def plot_sub_populations(sub_plot_data, num_sub_pop, gens, trial_num,  max_fit=True):
+    
+    index_increment = int(len(sub_plot_data[0][:])/num_sub_pop)
+    
+    mean_fitness = np.zeros((num_sub_pop, gens))
+    max_fitness = np.zeros((num_sub_pop, gens))
+    
+    for i in range(gens): # row
+        start_idx = 0
+        stop_idx = index_increment
+        for j in range(num_sub_pop):
+            fitness_current_generation = sub_plot_data[i][:]
+            mean_fitness[j][i] = np.mean(fitness_current_generation[start_idx:stop_idx])
+            max_fitness[j][i] = np.max(fitness_current_generation[start_idx:stop_idx])
+            start_idx += index_increment
+            stop_idx += index_increment
+    
+    generations = np.arange(1, gens + 1, 1)
+    colour = ['lightskyblue', 'blue','pink', 'orange', 'red']
+    
+    if max_fit:
+    
+        for x in range(num_sub_pop):
+
+            plt.plot(generations, max_fitness[x], linestyle='dotted' ,color=colour[x], label='max sub-population '+ str(x))
+            plt.title('Max Fitness Iteration '+str(trial_num))
+            
+    else: 
+        for x in range(num_sub_pop):
+            plt.plot(generations, mean_fitness[x], linestyle='dashed' ,color=colour[x], label='mean sub-population '+ str(x))
+            plt.title('Mean Fitness Iteration '+str(trial_num))
+
+    plt.legend()
+    plt.show()
+    
+    return
+        
+    
+    
