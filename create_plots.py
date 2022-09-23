b4672684f80a123a11b9588337bccc387f6d0ee3
agolombek <91390904+agolombek@users.noreply.github.com>
2022-09-23 16:20:15 +0200

    Update create_plots.py

--- create_plots.py
+++ create_plots.py
@@ -7,11 +7,12 @@ Created on Fri Sep 23 16:06:23 2022
 
 from plotting_functions import *
 
-colour = ['blue', 'red', 'green', 'orange']
+
+colour = ['blue', 'red', 'green', 'orange', 'purple', 'brown', 'pink', 'black', 'olive', 'cyan', 'yellow', 'navy', 'darkviolet', 'lime', 'firebrick', 'gray', 'peru', 'darkkhaki', 'slateblue', 'lightcoral']
 name_1 = 'TSP'
 name_2 = 'sigma'
 
-TSPs = [30]
+TSPs = [10, 30]
 sigmas = [0.05, 0.1, 0.15, 0.2]
 
 tuning_plot_mean_fitness(TSPs, sigmas, name_1, name_2, colour, std=False)
