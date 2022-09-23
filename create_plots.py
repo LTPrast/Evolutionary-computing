7e9edc77eff1d010820e5d92a31e4c255fd7fd41
agolombek <91390904+agolombek@users.noreply.github.com>
2022-09-24 00:30:49 +0200

    Update create_plots.py

--- create_plots.py
+++ create_plots.py
@@ -12,13 +12,13 @@ colour = ['blue', 'red', 'green', 'orange', 'purple', 'brown', 'pink', 'black', 
 name_1 = 'TSP'
 name_2 = 'sigma'
 
-TSPs = [10, 30]
+TSPs = [10, 20, 30]
 sigmas = [0.05, 0.1, 0.15, 0.2]
 
 # compare a set of trials for different paramter values, plot max and mean on seperate plot
 # also prints a sorted list from worst to best
-tuning_plot_mean_fitness(TSPs, sigmas, name_1, name_2, colour, std=False)
-tuning_plot_max_fitness(TSPs, sigmas, name_1, name_2, colour, std=False)
+# tuning_plot_mean_fitness(TSPs, sigmas, name_1, name_2, colour, std=False)
+# tuning_plot_max_fitness(TSPs, sigmas, name_1, name_2, colour, std=False)
 
 
 # 3D plot 
