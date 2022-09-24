813657242e524fdda8182e129856771f0b2f3dd4
agolombek <91390904+agolombek@users.noreply.github.com>
2022-09-24 22:05:54 +0200

    experiments

--- Central_Algorithm.py
+++ Central_Algorithm.py
@@ -26,7 +26,7 @@ from survival_methods import *
 
 ######################### SET-UP FRAMEWORK ###################################
 
-tournament_size = 10                    # Number of individuals taking part in tournamnet selection 
+tournament_size = 40                    # Number of individuals taking part in tournamnet selection 
 sigma = 0.175                           # gene mutation probability 
 
 # choose this for not using visuals and thus making experiments faster
