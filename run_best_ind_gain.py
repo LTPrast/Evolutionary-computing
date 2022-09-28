de576003b25f2085ba46dfdd6bc75949e31ac846
LizaRoelofsen <60733836+LizaRoelofsen@users.noreply.github.com>
2022-09-28 19:16:02 +0200

    boxplots and get gains of best solutions over 5 runs

--- run_best_ind_gain.py
+++ run_best_ind_gain.py
@@ -18,6 +18,7 @@ from math import fabs,sqrt
 import glob, os
 import matplotlib.pyplot as plt
 import pandas as pd
+import pickle
 
 # import evolutionary algorithm functions
 from parent_selection_methods import *
@@ -35,19 +36,19 @@ if headless:
 # runs simulation
 def simulation(env,x):
     f,p,e,t = env.play(pcont=x)
-    return f
+    return f,p,e
 
 # evaluates fitness of every individual in population
-def evaluate(x):
-    fit = []
-    player_hp = []
-    enemy_hp = []
-    for agent in x:
-        f, p, e = simulation(env, agent)
-        fit.append(f)
-        player_hp.append(p)
-        enemy_hp.append(e)
-    return np.array(fit), np.array(player_hp), np.array(enemy_hp)
+# def evaluate(x):
+#     fit = []
+#     player_hp = []
+#     enemy_hp = []
+#     for agent in x:
+#         f, p, e = simulation(env, agent)
+#         fit.append(f)
+#         player_hp.append(p)
+#         enemy_hp.append(e)
+#     return np.array(fit), np.array(player_hp), np.array(enemy_hp)
 
 
 ####################### SET EXPERIMENT PARAMETERS ###########################
@@ -81,19 +82,23 @@ for opponent in opponents:
 
         start_time = time.time()
 
-        # number of times for this experiment to be repeated
-        nr_trails = 10
-
         # create empty arrays to store values for every experiment
-        ind_gains = np.empty((0,10), float)
+        ind_gains = []
 
-        best_solutions = pd.read_csv(f'./{experiment_name}/{experiment_name}_highest_gain.csv',delimiter=",")
+        #best_solutions = pd.read_csv(f'./{experiment_name}/{experiment_name}_highest_gain.csv',delimiter=",")
+        best_solutions = pd.read_csv(f'./{experiment_name}/island_algo_enemy_{opponent[0]}_highest_gain.csv',delimiter=",")
+        best_solutions = best_solutions.to_numpy()
+        
+        # number of times for this experiment to be repeated
+        nr_trials = best_solutions.shape[0]
 
-        for i in range(nr_trails):
+        for i in range(nr_trials):
 
             gains = []
             for j in range(5):
-                gains.append(evaluate(best_solutions[i]))
+                agent_nn = np.array(best_solutions[i][1:],dtype=np.float32)
+                f, p, e = simulation(env,agent_nn)
+                gains.append(p - e)
             
             ind_gains.append(np.mean(gains))
 
@@ -102,24 +107,8 @@ for opponent in opponents:
         execution_time = ((end_time-start_time)/60)/60
         print("Runtime was ", execution_time, " hours")
 
-        ##################### Save Data in File ####################################
-
-        def save_file(data, file_name, experiment_name, cols=True, rows=True):
-            # Save the data into a dataframe and save in csv file
-            df = pd.DataFrame(data)
-
-            # if cols == True:
-            #     columns = ['Generation_'+str(i+1) for i in range(gens+1)]
-            #     df.columns = columns
-            # if rows == True:
-            #     rows = ['Trial_'+str(i+1) for i in range(experiment_iterations)]
-            #     df.index = rows
-            df.to_csv(experiment_name+'/'+experiment_name+file_name)
-            return
-
         # save value of runs
-        save_file(ind_gains, '_ind_gains.csv', experiment_name)
+        with open(experiment_name+'/'+experiment_name+'_ind_gains', "wb") as file:
+            pickle.dump(ind_gains, file)
 
-    comp_algos_boxplots(f'enemy_{opponent[0]}_{algos[0]}', f'enemy_{opponent[0]}_{algos[1]}')
-
-    
+    comp_algos_boxplots(f'{algos[0]}_algo_enemy_{opponent[0]}', f'{algos[1]}_algo_enemy_{opponent[0]}')
